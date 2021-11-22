bb08240170e17e6fa86444f0d7556eea1057141c
TanCodes <barvitanmay20@gmail.com>
2021-11-22 20:52:08 +0530

    new updated for hosting

--- repoloader/urls.py
+++ repoloader/urls.py
@@ -6,9 +6,13 @@ from django.contrib import admin
 from django.urls import path
 from repoloader import views
 
+# for heruko
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("get_repo", views.get_repo, name="get_repo"),
     path("downloaded_to_csv", views.downloaded_to_csv, name="downloaded_to_csv"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
