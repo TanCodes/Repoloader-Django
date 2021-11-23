5b553508ce86dfba4c7b656af4563640380cafcf
TanCodes <barvitanmay20@gmail.com>
2021-11-23 11:07:00 +0530

    updated

--- repoloader/settings.py
+++ repoloader/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "repoloader/static")]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# for Heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
